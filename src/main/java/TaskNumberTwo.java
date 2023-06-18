bff143ce31f6c3e4741b3c51c2c50cee2c54ad1b
admin <teplyakov.in@gmail.com>
2023-06-18 14:35:31 +0200

    Make second tusk

---
+++ src/main/java/TaskNumberTwo.java
@@ -0,0 +1,50 @@
+import java.util.regex.Pattern;
+
+public class TaskNumberTwo {
+    public static void main(String[] args){
+        getOutput(getData(fixData(provideData())));}
+
+
+
+    private static String[] provideData() {
+        return new String[]{"orange","plum","tomato","onion","grape","onion"};
+    }
+
+// Оставил метод fixData, чтобы в массиве удалить onion-ы так сказать физически, а не номинально,
+// хотя можно было просто вывести массив без них не затрагивая содержимое.
+    private  static  String[] fixData(String[] Data){
+        int count =0;
+        for (String name: Data) {
+
+
+            if (Pattern.matches("onion", name))
+
+                Data[count]= null;
+            count++;
+
+        }   return  Data;}
+        private static String getData(String[] Data) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = 0;
+
+            for (String name : Data) {
+
+            if (name!=null){
+                count++;
+                stringBuilder
+                        .append(count)
+                        .append(") ")
+                        .append(name)
+                        .append("\n");
+            }}
+
+            return stringBuilder.toString();
+        }
+        private static void getOutput(String output) {
+            System.out.println(output);
+
+
+
+        }
+}
